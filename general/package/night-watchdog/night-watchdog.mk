e7a7e80ef5101a7cd3c4681d6e1e33b109dddd26
joinme <282662403@qq.com>
2026-05-13 22:47:17 +0800

    Update night-watchdog.mk

--- general/package/night-watchdog/night-watchdog.mk
+++ general/package/night-watchdog/night-watchdog.mk
@@ -9,9 +9,11 @@ NIGHT_WATCHDOG_SITE = $(BR2_EXTERNAL)/general/package/night-watchdog/src
 NIGHT_WATCHDOG_SITE_METHOD = local
 NIGHT_WATCHDOG_LICENSE = GPL-2.0+
 
-# 这是一个纯脚本包，没有编译过程，直接定义安装命令
 define NIGHT_WATCHDOG_INSTALL_TARGET_CMDS
+	# 安装脚本到 /usr/bin
 	$(INSTALL) -D -m 0755 $(@D)/night_watchdog.sh $(TARGET_DIR)/usr/bin/night_watchdog.sh
+	# 安装启动脚本到 /etc/init.d
+	$(INSTALL) -D -m 0755 $(@D)/S99night $(TARGET_DIR)/etc/init.d/S99night
 endef
 
 $(eval $(generic-package))
