a694ad7efc51218dd50a1f78e94e01484f958b74
joinme <282662403@qq.com>
2026-05-13 21:52:11 +0800

    Update gpio-motors.c

--- general/package/gpio-motors/src/gpio-motors.c
+++ general/package/gpio-motors/src/gpio-motors.c
@@ -7,180 +7,199 @@
 
 int PAN_PINS[4];
 int TILT_PINS[4];
-int SELECT_PIN = -1;
+int H_ENABLE = -1;   // 水平使能
+int V_ENABLE = -1;   // 垂直使能
 
 int STEP_SEQUENCE[8][4] = {
-	{1, 0, 0, 0}, {1, 1, 0, 0}, {0, 1, 0, 0}, {0, 1, 1, 0},
-	{0, 0, 1, 0}, {0, 0, 1, 1}, {0, 0, 0, 1}, {1, 0, 0, 1}
+    {1, 0, 0, 0}, {1, 1, 0, 0}, {0, 1, 0, 0}, {0, 1, 1, 0},
+    {0, 0, 1, 0}, {0, 0, 1, 1}, {0, 0, 0, 1}, {1, 0, 0, 1}
 };
 
 int REVERSE_STEP_SEQUENCE[8][4] = {
-	{1, 0, 0, 1}, {0, 0, 0, 1}, {0, 0, 1, 1}, {0, 0, 1, 0},
-	{0, 1, 1, 0}, {0, 1, 0, 0}, {1, 1, 0, 0}, {1, 0, 0, 0}
+    {1, 0, 0, 1}, {0, 0, 0, 1}, {0, 0, 1, 1}, {0, 0, 1, 0},
+    {0, 1, 1, 0}, {0, 1, 0, 0}, {1, 1, 0, 0}, {1, 0, 0, 0}
 };
 
 void gpio_release(int pin) {
-	char path[64];
-	snprintf(path, sizeof(path), "/sys/class/gpio/gpio%d/value", pin);
-	FILE *file = fopen(path, "w");
-	if (file) {
-		fprintf(file, "%d", 0);
-		fclose(file);
-	}
-
-	file = fopen("/sys/class/gpio/unexport", "w");
-	if (file) {
-		fprintf(file, "%d", pin);
-		fclose(file);
-	} else {
-		printf("Unable to unexport GPIO %d: [%d] %s\n", pin, errno, strerror(errno));
-	}
+    if (pin < 0) return;
+    char path[64];
+    snprintf(path, sizeof(path), "/sys/class/gpio/gpio%d/value", pin);
+    FILE *file = fopen(path, "w");
+    if (file) {
+        fprintf(file, "0");
+        fclose(file);
+    }
+    file = fopen("/sys/class/gpio/unexport", "w");
+    if (file) {
+        fprintf(file, "%d", pin);
+        fclose(file);
+    }
 }
 
 void gpio_clean(int error) {
-	for (int i = 0; i < 4; i++) {
-		gpio_release(PAN_PINS[i]);
-		gpio_release(TILT_PINS[i]);
-	}
-
-	if (SELECT_PIN != -1) {
-		gpio_release(SELECT_PIN);
-	}
-
-	if (error) {
-		exit(EXIT_FAILURE);
-	}
+    for (int i = 0; i < 4; i++) {
+        gpio_release(PAN_PINS[i]);
+        gpio_release(TILT_PINS[i]);
+    }
+    gpio_release(H_ENABLE);
+    gpio_release(V_ENABLE);
+    if (error) exit(EXIT_FAILURE);
 }
 
 void gpio_export(int pin) {
-	char path[64];
-	FILE *file;
-
-	file = fopen("/sys/class/gpio/export", "w");
-	if (file) {
-		fprintf(file, "%d", pin);
-		fclose(file);
-	} else {
-		printf("Unable export GPIO %d: [%d] %s\n", pin, errno, strerror(errno));
-		gpio_clean(1);
-	}
-
-	snprintf(path, sizeof(path), "/sys/class/gpio/gpio%d/direction", pin);
-	file = fopen(path, "w");
-	if (file) {
-		fprintf(file, "out");
-		fclose(file);
-	} else {
-		printf("Unable to set direction of GPIO %d: [%d] %s\n", pin, errno, strerror(errno));
-		gpio_clean(1);
-	}
+    if (pin < 0) return;
+    char path[64];
+    FILE *file = fopen("/sys/class/gpio/export", "w");
+    if (file) {
+        fprintf(file, "%d", pin);
+        fclose(file);
+    } else {
+        printf("Unable to export GPIO %d: %s\n", pin, strerror(errno));
+        gpio_clean(1);
+    }
+    snprintf(path, sizeof(path), "/sys/class/gpio/gpio%d/direction", pin);
+    file = fopen(path, "w");
+    if (file) {
+        fprintf(file, "out");
+        fclose(file);
+    } else {
+        printf("Unable to set direction of GPIO %d: %s\n", pin, strerror(errno));
+        gpio_clean(1);
+    }
 }
 
 void gpio_set(int pin, int value) {
-	char path[64];
-	snprintf(path, sizeof(path), "/sys/class/gpio/gpio%d/value", pin);
-	FILE *file = fopen(path, "w");
-	if (file) {
-		fprintf(file, "%d", value);
-		fclose(file);
-	} else {
-		printf("Unable to set value of GPIO %d: [%d] %s\n", pin, errno, strerror(errno));
-		gpio_clean(1);
-	}
+    if (pin < 0) return;
+    char path[64];
+    snprintf(path, sizeof(path), "/sys/class/gpio/gpio%d/value", pin);
+    FILE *file = fopen(path, "w");
+    if (file) {
+        fprintf(file, "%d", value);
+        fclose(file);
+    } else {
+        printf("Unable to set value of GPIO %d: %s\n", pin, strerror(errno));
+        gpio_clean(1);
+    }
 }
 
 void gpio_config() {
-	FILE *fp = popen("fw_printenv -n gpio_motors", "r");
-	if (fp == NULL) {
-		printf("Unable to run fw_printenv\n");
-		exit(EXIT_FAILURE);
-	}
+    FILE *fp = popen("fw_printenv -n gpio_motors", "r");
+    if (!fp) {
+        printf("Unable to run fw_printenv\n");
+        exit(EXIT_FAILURE);
+    }
 
-	char line[64];
-	if (fgets(line, sizeof(line), fp) != NULL) {
-		char *token = strtok(line, " ");
-		int value[8];
-		int count = 0;
+    char line[64];
+    if (fgets(line, sizeof(line), fp) == NULL) {
+        printf("Error: Unable to read gpio_motors\n");
+        exit(EXIT_FAILURE);
+    }
+    pclose(fp);
 
-		while (token != NULL && count < 8) {
-			value[count++] = atoi(token);
-			token = strtok(NULL, " ");
-		}
+    int value[8];
+    int count = 0;
+    char *token = strtok(line, " ");
+    while (token != NULL && count < 8) {
+        value[count++] = atoi(token);
+        token = strtok(NULL, " ");
+    }
 
-		if (count == 8 || count == 5) {
-			for (int i = 0; i < 4; i++) {
-				PAN_PINS[i] = value[i];
-				TILT_PINS[i] = value[i + (count == 8 ? 4 : 0)];
-			}
-
-			if (count == 5) {
-				SELECT_PIN = value[4];
-			}
-		} else {
-			printf("Error: Expected 8 or 5 GPIO values, but got %d\n", count);
-			exit(EXIT_FAILURE);
-		}
-	} else {
-		printf("Error: Unable to read gpio_motors from fw_printenv\n");
-		exit(EXIT_FAILURE);
-	}
-
-	pclose(fp);
+    if (count == 6) {
+        // 6 参数模式：前 4 控制线，后 2 使能脚 (H_ENABLE, V_ENABLE)
+        for (int i = 0; i < 4; i++) {
+            PAN_PINS[i] = value[i];
+            TILT_PINS[i] = value[i];
+        }
+        H_ENABLE = value[4];
+        V_ENABLE = value[5];
+    } else if (count == 5) {
+        // 兼容旧 5 参数模式：单 SELECT_PIN，此时 TILT_PINS = PAN_PINS
+        for (int i = 0; i < 4; i++) {
+            PAN_PINS[i] = value[i];
+            TILT_PINS[i] = value[i];
+        }
+        // 将原来的 SELECT_PIN 同时赋值给 H_ENABLE 和 V_ENABLE
+        // 但硬件不匹配，只能打印警告
+        printf("Warning: 5-value config not suitable for dual enable pins. Use 6 values.\n");
+        H_ENABLE = value[4];
+        V_ENABLE = value[4];
+    } else if (count == 8) {
+        // 兼容 8 参数模式：前后 4 个分别用于 pan 和 tilt，无使能脚
+        for (int i = 0; i < 4; i++) {
+            PAN_PINS[i] = value[i];
+            TILT_PINS[i] = value[i + 4];
+        }
+        H_ENABLE = -1;
+        V_ENABLE = -1;
+    } else {
+        printf("Error: Expected 5, 6 or 8 GPIO values, got %d\n", count);
+        exit(EXIT_FAILURE);
+    }
 }
 
-void axis_run(const int pins[4], int level, int steps, int delay) {
-	int remaining = abs(steps);
-	if (remaining == 0) {
-		return;
-	}
+void axis_run(const int pins[4], int axis, int steps, int delay) {
+    if (steps == 0) return;
 
-	const int (*seq)[4] = (steps < 0) ? REVERSE_STEP_SEQUENCE : STEP_SEQUENCE;
-	if (SELECT_PIN != -1) {
-		gpio_set(SELECT_PIN, level);
-		usleep(100);
-	}
+    // 设置使能
+    if (axis == 0) {  // 水平
+        gpio_set(H_ENABLE, 1);
+        gpio_set(V_ENABLE, 0);
+    } else {          // 垂直
+        gpio_set(H_ENABLE, 0);
+        gpio_set(V_ENABLE, 1);
+    }
 
-	int micro = 0;
-	while (remaining > 0) {
-		for (int i = 0; i < 4; i++) {
-			gpio_set(pins[i], seq[micro][i]);
-		}
+    const int (*seq)[4] = (steps < 0) ? REVERSE_STEP_SEQUENCE : STEP_SEQUENCE;
+    int remaining = abs(steps);
+    int micro = 0;
 
-		usleep(delay);
-		if (++micro >= 8) {
-			micro = 0;
-			--remaining;
-		}
-	}
+    while (remaining > 0) {
+        for (int i = 0; i < 4; i++) {
+            gpio_set(pins[i], seq[micro][i]);
+        }
+        usleep(delay);
+        if (++micro >= 8) {
+            micro = 0;
+            remaining--;
+        }
+    }
 
-	for (int i = 0; i < 4; i++) {
-		gpio_set(pins[i], 0);
-	}
+    // 控制线归零
+    for (int i = 0; i < 4; i++) {
+        gpio_set(pins[i], 0);
+    }
+    // 关闭所有使能
+    gpio_set(H_ENABLE, 0);
+    gpio_set(V_ENABLE, 0);
 }
 
 int main(int argc, char *argv[]) {
-	if (argc != 4) {
-		fprintf(stderr, "Usage: %s <pan steps> <tilt steps> <delay (ms)>\n", argv[0]);
-		return 1;
-	}
+    if (argc != 4) {
+        fprintf(stderr, "Usage: %s <pan steps> <tilt steps> <delay (ms)>\n", argv[0]);
+        return 1;
+    }
 
-	int pan_steps = atoi(argv[1]);
-	int tilt_steps = atoi(argv[2]);
-	int delay = atoi(argv[3]) * 1000;
+    int pan_steps = atoi(argv[1]);
+    int tilt_steps = atoi(argv[2]);
+    int delay = atoi(argv[3]) * 1000;   // ms 转 us
 
-	gpio_config();
-	for (int i = 0; i < 4; i++) {
-		gpio_export(PAN_PINS[i]);
-		gpio_export(TILT_PINS[i]);
-	}
+    gpio_config();
 
-	if (SELECT_PIN != -1) {
-		gpio_export(SELECT_PIN);
-	}
+    // 导出所有用到的 GPIO
+    for (int i = 0; i < 4; i++) {
+        gpio_export(PAN_PINS[i]);
+        gpio_export(TILT_PINS[i]);
+    }
+    gpio_export(H_ENABLE);
+    gpio_export(V_ENABLE);
 
-	axis_run(PAN_PINS, 0, pan_steps, delay);
-	axis_run(TILT_PINS, 1, tilt_steps, delay);
-	gpio_clean(0);
+    // 初始化使能为低
+    gpio_set(H_ENABLE, 0);
+    gpio_set(V_ENABLE, 0);
 
-	return 0;
+    axis_run(PAN_PINS, 0, pan_steps, delay);
+    axis_run(TILT_PINS, 1, tilt_steps, delay);
+
+    gpio_clean(0);
+    return 0;
 }
