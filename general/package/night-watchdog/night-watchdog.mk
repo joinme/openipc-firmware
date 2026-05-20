84c876b8080e7620afb4e418d702dbc5088383c5
joinme <282662403@qq.com>
2026-05-20 19:05:16 +0800

    Update night-watchdog.mk

--- general/package/night-watchdog/night-watchdog.mk
+++ general/package/night-watchdog/night-watchdog.mk
@@ -14,6 +14,10 @@ define NIGHT_WATCHDOG_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0755 $(@D)/night_watchdog.sh $(TARGET_DIR)/usr/bin/night_watchdog.sh
 	# 安装启动脚本到 /etc/init.d
 	$(INSTALL) -D -m 0755 $(@D)/S99night $(TARGET_DIR)/etc/init.d/S99night
+  # 安装脚本到 /usr/bin
+	$(INSTALL) -D -m 0755 $(@D)/night_watchdog.sh $(TARGET_DIR)/usr/bin/append-ptz.sh
+	# 安装启动脚本到 /etc/init.d
+	$(INSTALL) -D -m 0755 $(@D)/S99night $(TARGET_DIR)/etc/init.d/S90ptz
 endef
 
 $(eval $(generic-package))
