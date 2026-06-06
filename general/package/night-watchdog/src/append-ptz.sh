d30e12f76f4fbc8d044de7815765ccbd9eb7e824
joinme <282662403@qq.com>
2026-06-06 21:37:51 +0800

    Update append-ptz.sh

--- general/package/night-watchdog/src/append-ptz.sh
+++ general/package/night-watchdog/src/append-ptz.sh
@@ -19,13 +19,21 @@ if [ -f /etc/majestic.yaml ]; then
 	sed -i '
 	  # ===== OSD 段 =====
 	  /^osd:/,/^[a-z]/ {
+	      # 启用 OSD
 	      s/^\([[:space:]]*\)enabled:[[:space:]]*false/\1enabled: true/
+	      
+	      # 修改时间格式模板
+	      s/^\([[:space:]]*\)template:[[:space:]]*'"'"'%d.%m.%Y %H:%M:%S'"'"'$/\1template: '"'"'%Y.%m.%d %A %H:%M:%S'"'"'/
+	      
+	      # 添加 size: 2 字段（如果不存在）
+	      /^\([[:space:]]*\)posY:[[:space:]]*16$/a\ \ \ size: 2
 	  }
 	
 	  # ===== Audio 段 =====
 	  /^audio:/,/^[a-z]/ {
 	      # 启用音频
 	      s/^\([[:space:]]*\)enabled:[[:space:]]*false/\1enabled: true/
+	      
 	      # 在 speakerPin: 15 之后插入 speakerPinInvert: false
 	      /^[[:space:]]*speakerPin:[[:space:]]*15$/a\  speakerPinInvert: false
 	  }
